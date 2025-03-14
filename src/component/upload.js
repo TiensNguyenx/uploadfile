5365ad21443e093fb5895f73d7713403f3524043
TiensNguyenx <huynhtien26032002@gmail.com>
2025-03-14 17:29:28 +0700

    update

--- src/component/upload.js
+++ src/component/upload.js
@@ -12,17 +12,17 @@ const [file, setFile] = useState(null);
 const [error, setError] = useState('');
 const fileInputRef = useRef(null);
 const [showModalLoading, setShowModalLoading] = useState(false);
+const [bookId, setBookId] = useState('');
 const handleFileChange = (e) => {
   const selectedFile = e.target.files[0];
   if (selectedFile) {
-    setFile(selectedFile);
+    setFile([selectedFile]);
     setError('');
   } else {
     setFile(null);
     setError('Please select a PDF file.');
   }
 };
-// const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const handleClose = () => {
   setShowModalLoading(false);
 }
@@ -32,8 +32,13 @@ const handleSubmit = async (e) => {
     setError('Please select a PDF file.');
     return;
   }
+  if(bookId === ''){
+    setError('Please enter your book ID.');
+    return;
+  }
   const formData = new FormData();
-  formData.append('file', file); // Key là 'file', value là file PDF
+  formData.append('file', file); 
+  formData.append('file_id', bookId);
   setShowModalLoading(true);
   axios.post('http://127.0.0.1:5000/upload', formData, {
     headers: { 
@@ -74,14 +79,27 @@ return (
 <button className={cx('button')} onClick={() => {
    setFile(null);
    if (fileInputRef.current) {
-     fileInputRef.current.value = ""; // Reset input file
+     fileInputRef.current.value = ""; 
    }
 }}>
 <MdDeleteForever fontSize='30px' color="red"/>
 </button>
+<div className={cx('form')}>
+<div className={cx('form-label')}>Book ID</div>
+
+        <input
+          type="text"
+          value={bookId}
+          onChange={(e) => setBookId(e.target.value)}
+          placeholder="Enter your book ID"
+          className={cx('form-input')}
+          required
+        />
+</div>
    </div>
 
-     <Button variant="primary" onClick={handleSubmit} size="lg" style={{padding:'5px 15px'}}><FaUpload style={{textAlign:'center', margin:'0 10px 6px 0'}}/>Upload</Button>
+     <Button variant='primary' disabled={!bookId || !fileInputRef.current?.value}
+ onClick={handleSubmit} size="lg" style={{padding:'5px 15px'}}><FaUpload style={{textAlign:'center', margin:'0 10px 6px 0'}}/>Upload</Button>
       {error && <p style={{color:'red',marginTop:'10px'}}>{error}</p>}
  </div>
 <Loading
